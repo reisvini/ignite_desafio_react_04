88a4ca9ff8a38c35dd266db644d1919a4fa09347
Vinicius Reis <vinicius.reis@boxti.com.br>
2021-10-13 21:13:32 -0300

    Finishing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,9 +15,22 @@ export function Header(): JSX.Element {
           mx="auto"
           px={20}
           py={6}
+          flexDir={['column', 'column', 'row']}
         >
-          <Image src="logo.svg" h={10} />
-          <Button onClick={() => onOpen()}>Adicionar imagem</Button>
+          <Image
+            src="logo.svg"
+            h={10}
+            onClick={() => window.location.reload()}
+            cursor="pointer"
+            my={['0.5rem', 0]}
+          />
+          <Button
+            onClick={() => onOpen()}
+            mt="0.5rem"
+            fontSize={['90%', '1rem']}
+          >
+            Adicionar imagem
+          </Button>
         </Flex>
       </Box>
 
--- src/components/Modal/AddImage.tsx
+++ src/components/Modal/AddImage.tsx
@@ -30,7 +30,7 @@ export function ModalAddImage({
 
         <ModalCloseButton />
 
-        <ModalBody px={60}>
+        <ModalBody px={['1rem', '1rem', 60]}>
           <FormAddImage closeModal={handleCloseModal} />
         </ModalBody>
       </ModalContent>
